42febb31afa9b6fee37cb9be27dfc8ca64cda875
Martin Grünheit <m.gruenheit@gmail.com>
2021-02-23 10:17:58 +0100

    interactiv

--- tutorial.py
+++ tutorial.py
@@ -1,2 +1,18 @@
-my_num = 5
-print(round(2.46))
+
+
+name= input("Whats your name: ")
+
+def say_hi(name):
+
+    print("Hallo " + name +" wie gehts? ") 
+
+def say_response(antwort):
+    print("dir gehts also: " + antwort+ " ich dachte mir schon, dass du das sagen wirst")
+
+
+say_hi(name)
+
+antwort= input("")
+
+say_response(antwort)
+
