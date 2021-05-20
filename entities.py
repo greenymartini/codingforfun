86bede3f624ec34800d3607b06772b09b4a015af
Meredith Thomas <meredityman@gmail.com>
2021-05-20 22:03:22 +0200

    Working start menu

---
+++ entities.py
@@ -0,0 +1,22 @@
+# import pygame
+# import random
+# import time
+
+# SCREEN_WIDTH=1000
+# SCREEN_HEIGHT=800
+
+# class Scene:
+
+#     def __init__(self):
+#         pass
+
+#     def update():
+#         pass
+
+# class MenuScene(Scene):
+
+#     def __init__(self):
+#         super().__init__()
+#         pass
+
+
